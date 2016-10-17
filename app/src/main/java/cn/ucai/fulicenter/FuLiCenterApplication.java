1956623c73f7e6f62ff4c517aa6685d3f3dffe71
liuran <1227228155@qq.com>
2016-10-17 18:19:19 +0800

    -m添加application和完善新品fragment

---
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -0,0 +1,10 @@
+package cn.ucai.fulicenter;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/10/17 0017.
+ */
+
+public class FuLiCenterApplication extends Application {
+}
