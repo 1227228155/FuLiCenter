ef8c1f0aad77037fe1b21dfb74d1b5ed449742f4
liuran <1227228155@qq.com>
2016-10-23 23:35:04 +0800

    登录完成，接收登录服务器返回的数据

--- app/src/main/java/cn/ucai/fulicenter/Activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/Activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package cn.ucai.fulicenter.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -10,6 +12,12 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.bean.User;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.net.OkHttpUtils;
+import cn.ucai.fulicenter.utils.CommonUtils;
+import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
 import cn.ucai.fulicenter.view.DisplayUtils;
 
@@ -21,10 +29,15 @@ public class LoginActivity extends BaseActivity {
     @BindView(R.id.password_et)
     EditText passwordEt;
 
+    String username;
+    String password ;
+    LoginActivity mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        mContext =this;
         super.onCreate(savedInstanceState);
     }
 
@@ -47,11 +60,66 @@ public class LoginActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_login:
+                checkedInput();
                 break;
             case R.id.login_register:
                 MFGT.gotoRegister(this);
                 break;
         }
+    }
+
+    private void checkedInput() {
+        username = nameEt.getText().toString().trim();
+        password = passwordEt.getText().toString().trim();
+        if (TextUtils.isEmpty(username)){
+            CommonUtils.showLongToast(R.string.user_name_connot_be_empty);
+            nameEt.requestFocus();
+            return;
+        }else  if (TextUtils.isEmpty(password)){
+            CommonUtils.showLongToast(R.string.password_connot_be_empty);
+            passwordEt.requestFocus();
+            return;
+        }
+        login();
+
+    }
+
+    private void login() {
+        final ProgressDialog pd =new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.logining));
+        pd.show();
+        NetDao.login(mContext, username, password, new OkHttpUtils.OnCompleteListener<Result>() {
+            @Override
+            public void onSuccess(Result result) {
+                pd.dismiss();
+                if (result==null){
+                    CommonUtils.showLongToast(R.string.login_fail);
+
+                }else {
+                    if (result.isRetMsg()){
+                        User user = (User) result.getRetData();
+                        L.e("user"+user);
+                        MFGT.finish(mContext);
+                    }else {
+                        if (result.getRetCode()==I.MSG_LOGIN_UNKNOW_USER){
+                            CommonUtils.showLongToast(R.string.login_fail_unknow_user);
+                            pd.dismiss();
+                        }else if (result.getRetCode()==I.MSG_LOGIN_ERROR_PASSWORD){
+                            CommonUtils.showLongToast(R.string.login_fail_error_password);
+                            pd.dismiss();
+                        }else {
+                            CommonUtils.showLongToast(R.string.login_fail);
+                            pd.dismiss();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                CommonUtils.showLongToast(error);
+            }
+        });
     }
 
     @Override
